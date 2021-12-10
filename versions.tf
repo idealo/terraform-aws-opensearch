bd03d0659a2d9f0ab08cfeb63397a5cf10ada6d0
Steve Teuber <steve.teuber@idealo.de>
2021-12-10 14:43:42 +0100

    Add phillbaker/elasticsearch to required providers

--- versions.tf
+++ versions.tf
@@ -1,10 +1,14 @@
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = ">= 1.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.65"
+    }
+    elasticsearch = {
+      source  = "phillbaker/elasticsearch"
+      version = ">= 1.6.0"
     }
   }
 }
