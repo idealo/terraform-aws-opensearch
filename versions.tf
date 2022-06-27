aea0065126277775eab3be9118b42d7a702c370e
Steve Teuber <steve.teuber@idealo.de>
2022-06-27 14:52:42 +0200

    build(deps): bump hashicorp/aws from >= 3.65 to >= 4.12.0

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.65"
+      version = ">= 4.12.0"
     }
     elasticsearch = {
       source  = "phillbaker/elasticsearch"
