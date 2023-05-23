7253458e6bc3f5e3f386c53edb2e76f07cc9ed8f
Steve Teuber <steve.teuber@idealo.de>
2023-05-23 14:55:39 +0200

    fix(role_mapping): ensure role mappings exists when using internal_user_database (#36)

--- role_mapping.tf
+++ role_mapping.tf
@@ -14,7 +14,7 @@ resource "elasticsearch_opensearch_roles_mapping" "role_mapping" {
 }
 
 resource "elasticsearch_opensearch_roles_mapping" "master_user_arn" {
-  for_each = var.master_user_arn == "" ? {} : {
+  for_each = var.advanced_security_options_internal_user_database_enabled ? {} : {
     for key in ["all_access", "security_manager"] :
     key => try(local.role_mappings[key], {})
   }
@@ -24,6 +24,21 @@ resource "elasticsearch_opensearch_roles_mapping" "master_user_arn" {
   backend_roles = concat(try(each.value.backend_roles, []), [var.master_user_arn])
   hosts         = try(each.value.hosts, [])
   users         = try(each.value.users, [])
+
+  depends_on = [aws_route53_record.opensearch]
+}
+
+resource "elasticsearch_opensearch_roles_mapping" "master_user_name" {
+  for_each = var.advanced_security_options_internal_user_database_enabled ? {
+    for key in ["all_access", "security_manager"] :
+    key => try(local.role_mappings[key], {})
+  } : {}
+
+  role_name     = each.key
+  description   = try(each.value.description, "")
+  backend_roles = try(each.value.backend_roles, [])
+  hosts         = try(each.value.hosts, [])
+  users         = concat(try(each.value.users, []), [var.advanced_security_options_master_user_name])
 
   depends_on = [aws_route53_record.opensearch]
 }
