c58cef1bb9341303a6032a4b3c5ab75c76fe43fd
Steve Teuber <steve.teuber@idealo.de>
2023-12-05 11:02:36 +0100

    fix(depends_on): add missing role mapping dependencies (#60)

--- composable_index_template.tf
+++ composable_index_template.tf
@@ -6,6 +6,7 @@ resource "elasticsearch_composable_index_template" "composable_index_template" {
 
   depends_on = [
     elasticsearch_opensearch_roles_mapping.master_user_arn,
+    elasticsearch_opensearch_roles_mapping.master_user_name,
     aws_route53_record.opensearch
   ]
 }
